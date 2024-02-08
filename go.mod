913e1f53f4fd273ba42d53dc45be4ac7111db409
101-Legion <kildyaev.ivan@gmail.com>
2024-02-08 22:07:44 +0300

    Replace encoding/json on sonic and add general Logger

--- go.mod
+++ go.mod
@@ -3,15 +3,24 @@ module github.com/DevDrift/investment-game
 go 1.18
 
 require (
+	github.com/bytedance/sonic v1.10.2
 	github.com/google/uuid v1.5.0
 	github.com/joho/godotenv v1.5.1
+	github.com/rs/zerolog v1.31.0
+	github.com/stretchr/testify v1.8.4
 	go.etcd.io/bbolt v1.3.8
 )
 
 require (
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/integration/logger.go
@@ -0,0 +1,47 @@
+package integration
+
+import (
+	"os"
+	"fmt"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type Logger zerolog.Logger
+
+// InitLogger returns a Logger
+func InitLogger() *Logger {
+	newZerolog := zerolog.New(os.Stderr)
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if os.Getenv("DEBUG") == "true" {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	zerolog.GlobalLevel()
+	logger := Logger(newZerolog)
+
+	return &logger
+}
+
+// GetLevel returns the current Level.
+func (t *Logger)getGlobalLevel() zerolog.Level {
+	return zerolog.GlobalLevel()
+}
+
+// Debug sends a log event using debug level.
+func (t *Logger) Debug(args ...interface{}) {
+	log.Debug().Msg(fmt.Sprint(args...))
+}
+
+// Info sends a log event using info level.
+func (t *Logger) Info(args ...interface{}) {
+	log.Info().Msg(fmt.Sprint(args...))
+}
+
+// Error sends a log event using error level.
+func (t *Logger) Error(err error, args ...interface{}) {
+	log.Error().Err(err).Msg(fmt.Sprint(args...))
+}
